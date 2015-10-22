4aa531bfd90c6d34e93bd88a052cafde3e9bb9c2
Diorsdoge <393233652@qq.com>
2015-10-22 10:57:52 +0800

    add ch05 ing
    抄完5.1

---
+++ ch05/ex01.js
@@ -0,0 +1,51 @@
+//5 引用类型
+//5.1 Object类型
+var person = new Object();
+person.name = "Nicholas";
+person.age = 29;
+
+var person = {
+	name: "Nicholas",
+	age: 29
+};
+
+var person = {
+	name: "Nicholas",
+	age: 29,
+	5 : true  //数值自动转为字符串
+};
+
+var person = {};
+person.name = "Nicholas";
+person.age = 29;
+
+function displayInfo(args) {
+	var output = "";
+
+	if (typeof args.name == "string") {
+		output += "Name: " + args.name + "\n";
+	}
+
+	if (typeof args.age == "number") {
+		output += "Age: " + args.age + "\n";
+	}
+
+	alert(output);
+}
+displayInfo({
+	name: "Nicholas",
+	age: 29
+});
+displayInfo({
+	name: "Greg"
+});
+
+alert(person["name"]);  //"Nicholas"
+alert(person.name);  //"Nicholas"
+
+
+
+
+
+
+

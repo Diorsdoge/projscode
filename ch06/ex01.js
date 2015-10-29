9920eaf881cd75c091e6e97880e24082c113b459
Diorsdoge <393233652@qq.com>
2015-10-29 22:19:35 +0800

    。。。。
    。。。。。

--- ch06/ex01.js
+++ ch06/ex01.js
@@ -47,3 +47,118 @@ alert(person.name);	//"Nicholas"
 delete person.name;
 alert(person.name);	//"Nocholas"
 //configurable设置为false,表示不能从对象中删除属性
+
+//2. 访问器属性
+var book = {
+	_year: 2004,    //表示只能通过对象的方法访问的属性
+	edition: 1
+};
+Object.defineProperty(book, "year", {
+	get: function(){
+		return this._year;
+	},
+	set: function(newValue){
+		if (newValue > 2004) {
+			this._year = newValue;
+			this.edition += newValue - 2004;
+		}
+	}
+});
+book.year = 2005;
+alert(book.edition);		//2
+
+
+var book = {
+	_year: 2004,
+	edition: 1
+};
+//定义访问旧方法
+book.__defineGetter__("year", function(){
+	return this.year;
+});
+book.__defineGetter__("year", function(newValue){
+	if (newValue > 2004) {
+		this._year = newValue;
+		this.edition += newValue - 2004;
+	}
+});
+book.year = 2005;
+alert(book.edition);		//2
+
+//6.1.2 定义多个属性
+var book = {};
+Object.defineProperties(book, {
+	_year: {
+		value: 2004
+	},
+	edition: {
+		value: 1
+	},
+	year: {
+		get: function(){
+			return this._year;
+		},
+		set: function(){
+			if(newValue > 2004) {
+				this._year = newValue;
+				this.edition += newValue - 2004;
+			}
+		}
+	}
+});
+
+//6.1.2 读取属性的特性
+var book = {};
+Object.defineProperties(book, {
+	_year: {
+		value: 2004
+	},
+	edition: {
+		value: 1
+	},
+	year: {
+		get: function(){
+			return this._year;
+		},
+		set: function(newValue){
+			if (newValue > 2004) {
+				this._year = newValue;
+				this.edition += newValue - 2004;
+			}
+		}
+	}
+});
+var descriptor = Object.getOwnPropertyDescriptor(book, "_year");
+alert(descriptor.value);		//2004
+alert(descriptor.configurable);	//false
+alert(typeof descriptor.get);	//"undefined"
+
+var descriptor = Object.getOwnPropertyDescriptor(book, "year");
+alert(descriptor.value);		//undefined
+alert(descriptor.enumerable);	//false
+alert(typeof descriptor.get);	//"function"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
